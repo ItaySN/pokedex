6e1d42e6a6e962ffc59516ce60d9eaa350dff7fc
ItaySN <itayfic@gmail.com>
2020-08-03 14:51:58 +0300

    create an evene listener for the search button

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-const searchPokemon = async (pokemonId = 3) => {
+const searchInput = document.getElementById("search");
+searchInput.placeholder="Search..."
+const searchButton = document.getElementById("searchButton");
+searchButton.addEventListener('click',()=> searchButton(searchInput.value));
+
+
+
+const searchPokemon = async (pokemonId) => {
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   console.log(data)
 };
+
 searchPokemon();
 
