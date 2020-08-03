a6ebf300cbac416df27c4f48181e43a71a83ebad
ItaySN <itayfic@gmail.com>
2020-08-03 18:42:51 +0300

    Show list of types names for each pockemon
    with the for loop I run all over the typesArr of the pokemon from the data which I have recieved with the response , add them to the tempPoke arr and display them on the DOM.

--- main.js
+++ main.js
@@ -6,24 +6,37 @@ const containerDiv = document.getElementById("containerDiv");
 
 function createContainer(data)
 {
-  
  const tempPoke = [ document.createElement('div') , 
                     document.createElement('div') , 
                     document.createElement('div'), 
-                    document.createElement('img')];
+                    document.createElement('img')
+                  
+                  ];
   tempPoke[0].innerHTML = `name :   ${data.name}`; 
   tempPoke[1].innerHTML = `height :  ${data.height}` ; 
   tempPoke[2].innerHTML = `weight :   ${data.weight}`; 
   tempPoke[3].src = data.sprites.front_default;
   tempPoke[3].addEventListener('mouseover',()=>{tempPoke[3].src=data.sprites.back_default});
   tempPoke[3].addEventListener('mouseout',()=>{tempPoke[3].src=data.sprites.front_default})
+
+  //types:
+  for(let i=0;i<data.types.length; i++){
+    let divType = document.createElement('div');
+    divType.innerHTML = `type ${i+1} :  ${data.types[i].type.name}`;
+    tempPoke.push(divType);
+  }
   tempPoke.forEach(element=>{containerDiv.appendChild(element)});
+  let hr = document.createElement('hr');
+  containerDiv.appendChild(hr);
+  
+  
 }
 
 
 const searchPokemon = async (pokemonId) => {
   try{
     const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    console.log(data);
   createContainer(data);
   }
   catch(e){
